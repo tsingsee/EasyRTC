93632ef0c20167151b59e0609c758761acb7e475
liyy <1290374862@qq.com>
2019-01-10 23:10:00 +0800

    iOS的sdk和文档

---
+++ venus.framework/Headers/Room.h
@@ -0,0 +1,50 @@
+//
+//  Room.h
+//  venus
+//
+//  Created by Jac Chen on 2018/8/1.
+//  Copyright © 2018 Matrix. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Options.h"
+#import "SignalChannel.h"
+#import "InviteParameter.h"
+#import "WebRTCClient.h"
+#import "UserInfo.h"
+
+typedef NS_ENUM(NSUInteger, RoomStatus) {
+    ROOM_STATUS_SIGNOUT,
+    ROOM_STATUS_SIGNING,
+    ROOM_STATUS_SIGNIN,
+    ROOM_STATUS_CONNECTING,
+    ROOM_STATUS_CONNECTED,
+    ROOM_STATUS_DISCONNECTING,
+};
+
+@protocol RoomStatusDelegate <NSObject>
+
+- (void)onRoomStatusChange:(RoomStatus)roomStatus;
+- (void)onRoomUserJoin:(UserInfo *)info;
+- (void)onRoomUserLeave:(UserInfo *)info;
+- (void)onRoomUserModify:(UserInfo *)info;
+
+@end
+
+@interface Room : NSObject
+
+@property (nonatomic, strong) Options *options;
+@property (nonatomic, assign, readonly) RoomStatus roomStatus;
+@property (nonatomic, weak) id<RoomStatusDelegate> roomDelegate;
+
+- (NSInteger)join;
+- (void)leave;
+- (NSMutableArray *)getUserInfoList;
+
+- (void)muteClick:(UserInfo *)info;
+- (void)loudspeakerClick;
+
+- (BOOL)isSpeakerEnable;
+- (UIView *)getRenderView;
+
+@end
